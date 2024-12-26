21ee776192d99d89297c26fea5b6f46829fb98ca
MekJohn <gaudio90@hotmail.it>
2024-12-26 10:44:48 +0100

    field client update

--- src/client.py
+++ src/client.py
@@ -9,42 +9,42 @@ import endpoints as enp
 # Insert your api key here.
 
 class Auth:
-    
+
 
     DOCS = r"https://streak.readme.io/reference/get-current-user"
     ENDP = r"https://api.streak.com/api"
 
     def __init__(self, auth64: bytes) -> object:
-        self._auth64: bytes = auth64     
+        self._auth64: bytes = auth64
         self._log = [(tm.time_ns(), "START", "", None)]
-        
-    
+
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<Auth '{self.key.decode()}'>"
-    
+
     def __getitem__(self, item):
         return self._log[item] if item < len(self._log) else None
-    
+
     @property
     def is_connected(self):
         enpoint = enp.USER.ME
         response = self.get(enpoint)
         return True if response.status_code == 200 else False
-    
+
     @property
     def log(self):
         log_columns = "Time", "Type", "Url", "Response"
         table = pd.DataFrame(data=self._log,columns=log_columns)
         return table
-        
+
     @property
     def key(self):
         return self._auth64
-    
-        
+
+
     @classmethod
     def connect(cls, key_filepath: str = ""):
         key64 = cls._from_file(key_filepath)
@@ -53,16 +53,16 @@ class Auth:
 
     def _logger(self, *data):
         self._log.append(data)
-        
 
-    def get(self, url: str) -> req.models.Response:        
+
+    def get(self, url: str) -> req.models.Response:
         query = {"authorization": f"Basic {self.key.decode()}",
                  "accept": "application/json",
                  "Content-Type": "application/json"}
         response = req.get(url, headers = query)
         self._logger(tm.time_ns(), "GET", url, response)
         return response
-    
+
     def post(self, url, payload: dict = None):
         query = {"authorization": f"Basic {self.key.decode()}",
                  "accept": "application/json",
@@ -78,30 +78,30 @@ class Auth:
         encrypted_key = base64.b64encode(key.encode())
         return encrypted_key
 
-            
 
-    
-    
+
+
+
 class UserAPI:
-    
+
     _resources = enp.USER
-    
+
     def __init__(self, auth: object, user_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = user_data
         self.email = self.data["email"]
         self.key = self.data["key"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-        
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<UserAPI '{self.email}'>"
-    
+
     @classmethod
     def me(cls, auth):
         end_point = cls._resources.ME
@@ -116,39 +116,39 @@ class UserAPI:
         response = auth.get(end_point)
         if response.status_code == 200:
             return cls(auth, response.json())
-    
+
     @staticmethod
     def team(auth):
         return TeamAPI.my(auth)
-        
+
 
 class TeamAPI:
-    
+
     _resources = enp.TEAM
-    
+
     def __init__(self, auth: object, team_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = team_data
         self.name = self.data["name"]
         self.key = self.data["key"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<TeamAPI '{self.name}'>"
-    
+
     @classmethod
     def my(cls, auth):
         end_point = cls._resources.MY
         response = auth.get(end_point)
         if response.status_code == 200:
-            keys_to_keep = ("creationDate", "creator", 
-                            "name", "key", "members", 
+            keys_to_keep = ("creationDate", "creator",
+                            "name", "key", "members",
                             "lastSavedTimestamp")
             all_data = response.json()["results"][0]
             data = {k: v for k,v in all_data.items() if k in keys_to_keep}
@@ -156,27 +156,27 @@ class TeamAPI:
 
 
 
-class PipelineAPI:    
+class PipelineAPI:
 
     _resources = enp.PIPELINE
-    
+
     def __init__(self, auth: object, pipeline_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = pipeline_data
         self.name = self.data["name"]
         self.key = self.data["key"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<PipelineAPI '{self.name}'>"
-    
-    
+
+
     @classmethod
     def list(cls, auth: object) -> object:
         end_point = cls._resources.LIST
@@ -195,25 +195,25 @@ class PipelineAPI:
             return cls(auth, pip)
 
 class FieldAPI:
-    
+
     _resources = enp.FIELD
-    
+
     def __init__(self, auth: object, field_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = field_data
         self.name = self.data["name"]
         self.key = self.data["key"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<FieldAPI '{self.name}'>"
-    
+
     @classmethod
     def get(cls, auth: object, pipeline_key: str, field_key: str):
         end_point = cls._resources.GET
@@ -223,16 +223,16 @@ class FieldAPI:
         if response.status_code == 200:
             field_data = response.json()
             return cls(auth, field_data)
-    
+
     @classmethod
-    def update(cls, auth: object, box_key: str, field_key: str, 
+    def update(cls, auth: object, box_key: str, field_key: str,
                new_value: str):
         end_point = cls._resources.UPDATE
         end_point = end_point.format(box_key = box_key, field_key = field_key)
         response = auth.post(end_point, payload = {"value": new_value})
         if response.status_code == 200:
             return response
-    
+
     @classmethod
     def list(cls, auth: object, pipeline_key: str):
         end_point = cls._resources.LIST
@@ -243,49 +243,59 @@ class FieldAPI:
             for field in field_list:
                 yield cls(auth, field)
 
+    @classmethod
+    def listall(cls, auth) -> list[object]:
+        fields = dict()
+        for pip in PipelineAPI.list(auth):
+            fields.update({pip.key: pip["fields"]})
+        return fields
+
+
+
+
 
 class BoxAPI:
-    
+
     _resources = enp.BOX
-    
+
     def __init__(self, auth: object, box_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = box_data
-        
+
         self.name = self.data["name"]
         self.key = self.data["boxKey"]
-        
+
 
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<BoxAPI '{self.name}'>"
-    
-    
+
+
     @classmethod
     def get_by_key(cls, auth: object, box_key: str):
         end_point = cls._resources.GET_BY_KEY.format(box_key = box_key)
         response = auth.get(end_point)
         if response.status_code == 200:
             return cls(auth, response.json())
-    
+
     @classmethod
-    def get_by_name(cls, auth, box_name: str, 
+    def get_by_name(cls, auth, box_name: str,
                     stage_key: str = None, pipeline_key: str = None):
         end_point = cls._resources._GBN_HEAD
         end_point += "" if pipeline_key is None else cls._resources._GBN_IN_PIP
         end_point += "" if stage_key is None else cls._resources._GBN_AT_STG
         end_point += cls._resources._GBN_TAIL
-        
-        end_point = end_point.format(box_name = box_name, 
-                                     pipeline_key = pipeline_key, 
+
+        end_point = end_point.format(box_name = box_name,
+                                     pipeline_key = pipeline_key,
                                      stage_key = stage_key)
-        
+
         search_response = auth.get(end_point)
         if search_response.status_code == 200:
             boxlist = search_response.json()["results"]["boxes"]
@@ -294,35 +304,35 @@ class BoxAPI:
                 yield cls.get_by_key(auth, box_data)
 
     @classmethod
-    def list(cls, auth: object, pipeline_key: str, page: int = None, 
+    def list(cls, auth: object, pipeline_key: str, page: int = None,
              stage_key: str = None, limit: int = 10):
         """
         To limit the response, 'limit' parameter need to be set.
         The 'page' parameter than can be used to move around the response.
-        
-        If no page is set while limiting, the complete response will be 
+
+        If no page is set while limiting, the complete response will be
         retrieved by the api. Same when 'page' is set but 'limit' is none.
-        
+
         Note:
             no 'hasNextPage' attribute found as mentioned in the api docs.
         """
-        
+
         raw_ep = cls._resources._LIST_HEAD
         raw_ep += "" if page is None else cls._resources._LST_OPT_PAGE
         raw_ep += "" if stage_key is None else cls._resources._LST_OPT_STAGE
         raw_ep += "" if limit is None else cls._resources._LST_OPT_LIMIT
-        
+
         end_point = raw_ep.format(pipeline_key = pipeline_key,
-                                  page = page, stage_key = stage_key, 
+                                  page = page, stage_key = stage_key,
                                   limit = limit)
-        
-    
+
+
         response = auth.get(end_point)
         if response.status_code == 200:
             boxlist = response.json()
             for box_data in boxlist:
                 yield cls(auth, box_data)
-    
+
     @classmethod
     def files(cls, auth: object, box_key: str):
         end_point = cls._resources.FILES
@@ -333,35 +343,35 @@ class BoxAPI:
             for file in filelist:
                 # BUG TODO -- Endpoint
                 yield FileAPI.get(auth, file["fileKey"])
-    
+
     def is_box_key(self, key: str):
         return True if self.get_box(key).status_code == 200 else False
-    
+
 
 class ThreadAPI:
-    
+
     _resources = enp.THREAD
-    
+
     def __init__(self, auth: object, thread_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = thread_data
         self.name = self.data["subject"]
         self.key = self.data["key"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         subject = self.name
         if len(subject) > 20:
             subject = f"{self.name[:8]}...{self.name[-8:]}"
         return f"<ThreadAPI '{subject}'>"
-    
-    
+
+
     @classmethod
     def get(cls, auth: object, thread_key: str):
         end_point = cls._resources.GET
@@ -380,32 +390,32 @@ class ThreadAPI:
             threads = response.json()
             for thread in threads:
                 yield cls(auth, thread)
-        
+
 
 
 class FileAPI:
-    
+
     _resources = enp.FILE
-    
+
     def __init__(self, auth: object, file_data: dict):
         self.timestamp_ns = tm.time_ns()
         self.auth = auth
         self.data = file_data
         self.name = self.data["fileName"]
         self.key = self.data["fileKey"]
-        
+
     def __getitem__(self, item):
         return self.data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.key}"
-    
+
     def __repr__(self):
         return f"<FileAPI '{self.name}'>"
-    
+
     def __bytes__(self):
         return self.content(self.auth, self.key)
-    
+
     @classmethod
     def get(cls, auth: object, file_key: str):
         end_point = cls._resources.GET
@@ -414,19 +424,19 @@ class FileAPI:
         if response.status_code == 200:
             file_data = response.json()
             return cls(auth, file_data)
-    
+
     @classmethod
     def list(cls, auth: object, box_key: str):
         # BUG TODO -- Endpoint
-        # BoxAPI.files return different type of dict respect to what 
+        # BoxAPI.files return different type of dict respect to what
         # returned by FileAPI.list endpoint. Than for the moment,
         # the additional data on BoxAPI.files object is omitted
         # and GET endpoint is called instead of use the first directly
         for file in BoxAPI.files(auth, box_key):
             yield cls.get(auth, file["fileKey"])
-    
+
     @classmethod
-    def content(cls, auth: object, file_key: str) -> bytes:     
+    def content(cls, auth: object, file_key: str) -> bytes:
         end_point = cls._resources.CONTENT
         end_point = end_point.format(file_key = file_key)
         response = auth.get(end_point)
--- src/streaky.py
+++ src/streaky.py
@@ -4,14 +4,14 @@ import io
 import re
 
 import client as cl
-    
+
 
 class User:
-    
+
     api = cl.UserAPI
 
     def __init__(self, data: dict) -> object:
-        
+
         for k, v in data.items():
             setattr(self, k, v)
 
@@ -41,40 +41,40 @@ class User:
 
 
 class Pipeline:
-    
+
     api = cl.PipelineAPI
 
     def __init__(self, pipeline_response: object):
-        
-        self.response = pipeline_response   
-        
+
+        self.response = pipeline_response
+
         self.auth = self.response.auth
         self.key = self.response.key
         self.name = self.response.name
-        
+
         self.stages = self.response.data["stages"]
         self.fields = self.response.data["fields"]
-        self.last_update = self.response.data["lastSavedTimestamp"]
-        
+        self.timestamp = self.response.data["lastSavedTimestamp"]
+
     def __str__(self):
         return f"{self.name}"
 
     def __repr__(self):
         return f"<Pipeline '{self.name}'>"
-    
+
     def __getitem__(self, item: str):
         return self.response.get(item, None)
-    
+
     def __len__(self):
         return self.response.data["boxCount"]
-           
+
     def __contains__(self, box: str | object):
         pass
-    
+
 
     @classmethod
     def request(cls, auth: object, name: str = None, key: str = None):
-        
+
         if key is not None:
            pip_response = cls.api.get(auth, key)
            if pip_response is not None:
@@ -85,26 +85,34 @@ class Pipeline:
                     return cls(pip_response)
         else:
             return None
-    
-    
+
+
 
 class Box:
-    
+
     api = cl.BoxAPI
 
-    def __init__(self, box_response: object, pipeline: object = None):
-        
-        self.response = box_response   
-        
+    def __init__(self, box_response: object, pipeline: object):
+
+        self.response = box_response
+
         self.auth = self.response.auth
         self.key = self.response.key
         self.name = self.response.name
-            
+
+        self.parent = pipeline
         self.stage = self.response.data["stageKey"]
         self.fields = self.response.data["fields"]
-        
+        # TODO as properties
+        self.emails = None
+        self.tasks = None
+        self.calls = None
+        self.meetings = None
+
+        # Other
         self.notes = self.response.data["notes"]
         self.last_update = self.response.data["lastSavedTimestamp"]
+
 
 
     def __str__(self):
@@ -112,21 +120,15 @@ class Box:
 
     def __repr__(self):
         return f"<Box '{self.name}'>"
-    
-    
-    def __getitem__(self, item):
-        return self._data.get(item, None)
-    
-    def __contains__(self, other: str | object):
-        pass
-    
+
+
     def files(self):
         for file in self.response.files:
             yield file
-    
+
     @classmethod
     def request(cls, pipeline: object, name: str = None, key: str = None):
-        
+
         if key is not None:
            box_rep = cls.api.get(pipeline.auth, key)
            if box_rep is not None:
@@ -135,25 +137,26 @@ class Box:
             for box_rep in cls.api.get_by_name(pipeline.auth, name,
                                                pipeline_key = pipeline.key):
                 if box_rep.name.lower() == name.lower():
-                    return cls(box_rep, pipeline = pipeline)
+                    return cls(box_rep, pipeline)
         else:
             return None
-    
+
 
 class Field:
-    
+
     api = cl.FieldAPI
-    
-    def __init__(self, field_response: object, box: object = None):
-        pass
-        
+
+    def __init__(self, key: str = None, response: object = None):
+        if response is None:
+            response = self.request()
+
 
     def __getitem__(self, item):
         return self._data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.name}"
-    
+
     def __repr__(self):
         return f"<Field '{self.name}'>"
 
@@ -161,129 +164,133 @@ class Field:
 
     @classmethod
     def request(cls, pipeline: object, name: str = None, key: str = None):
-        
+
         if key is not None:
            field_rep = cls.api.get(pipeline.auth, key)
            if field_rep is not None:
                return cls(field_rep)
         elif name is not None:
-            for field_rep in cls.api.list(pipeline.auth, 
+            for field_rep in cls.api.list(pipeline.auth,
                                           pipeline.key, limit=None):
                 if field_rep.name.lower() == name.lower():
                     return cls(field_rep)
         else:
             return None
-    
+
+    def update(self, value):
+
+
+
 
 
 
 class File:
-    
+
     api = cl.FileAPI
-    
+
     def __init__(self, file: object):
 
         self._data = file_object
         self.key = file_object.key
-        
-    
+
+
     def __getitem__(self, item):
         return self._data.get(item, None)
-    
+
     def __str__(self):
         return f"{self.name}"
-    
+
     def __repr__(self):
         return f"<File '{self.name}'>"
-    
+
     @property
     def key(self):
         return self["fileKey"]
-    
+
     @property
     def name(self):
         return self["fileName"]
-    
+
     @property
     def stem(self):
         return os.path.splitext(self.name)[0]
-    
+
     @property
     def ext(self):
         return os.path.splitext(self.name)[1]
-       
+
     @property
     def last_update(self):
         return self["lastUpdatedTimestamp"]
-    
+
     @property
     def size(self):
         return self["size"]
-    
+
     @property
     def content(self):
         content = io.BytesIO(self._auth.get_file_content(self.key).content)
         return content
-    
-        
-        
+
+
+
 
 
 class Automa:
-    
+
     @staticmethod
     def connect(keyfile: str | object) -> object:
-        return cl.Auth.connect(keyfile)        
-    
-    @staticmethod
-    def last_price(box: object) -> float:
-        
-        REGEX = r"Signature\s€\s(?P<deal>.*)Impo"
-        
-        for file in box.files:
-            document = pp.PdfReader(file.content)
-            text = document.pages[0].extract_text()
-            pattern = re.compile(REGEX, re.DOTALL)
-            result = pattern.search(text).groupdict()["deal"]
-            if result is not None:
-                value = result.groupdict().get("deal", "")
-                cleaned = result.replace(".", "_").replace(",", ".")
-                value = float(cleaned)
-        return value
-        
+        return cl.Auth.connect(keyfile)
 
-# class InfoParser:
-    
-#     @staticmethod
-#     def search(name: str, pipeline: str = None, box: str, )
-    
-        
+    @staticmethod
+    def prices(box: object) -> dict:
+
+        REGEX = r"Signature\s€\s(?P<deal>.*)Impo"
+
+        prices = dict()
+        for file in box.files:
+            if file.ext == "pdf":
+                try:
+                    document = pp.PdfReader(file.content)
+                    text = document.pages[0].extract_text()
+                    pattern = re.compile(REGEX, re.DOTALL)
+                    result = pattern.search(text).groupdict()["deal"]
+                    if result is not None:
+                        value = result.groupdict().get("deal", "")
+                        cleaned = result.replace(".", "_").replace(",", ".")
+                        value = float(cleaned)
+                        prices.update({file.name: value})
+                except Exception as err:
+                    print(err)
+        return prices
+
+
 
 
 # class Supervisor:
-    
+
 #     @staticmethod
 #     def update_deal(box: object):
-        
+
 #         files = [f for f in box.files if f["fileName"].startswith("MKP-F")]
 #         if files != []:
 #             sorted_files = sorted(files, key = lambda x: x["lastSavedTimestamp"], reverse = True)[0]
 #             filedata = streak.get_file_content(sorted_files["fileKey"]).content
 #             filedata = io.BytesIO(filedata)
 #             document = pp.PdfReader(filedata)
-    
+
 #             text = document.pages[0].extract_text()
-    
+
 #             pattern = re.compile(r"Signature\s€\s(?P<deal>.*)Impo", re.DOTALL)
 #             found = float(pattern.search(text).groupdict()["deal"].replace(".", "_").replace(",", "."))
 #             field = [fd for fd in Pipeline(box.auth, key = box["pipelineKey"]).fields if fd["name"] == "Deal"][0]
 #             field_key = field["key"]
 #             print(f"Inserting '{found}' in the field '{field['name']}")
 #             response = box.auth.update_field_value(box.key, field_key, str(found))
-            
+
 #             return response
 
-        
+
 
 
 
